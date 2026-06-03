1ffa9a2f122a20a7d48d299ad9064bbecd2cfd4b
TG Optimization <beatshandsome@gmail.com>
2026-06-03 11:55:01 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'tg-material-v2';
+const urlsToCache = ['/', '/index.html', '/manifest.json'];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+});
